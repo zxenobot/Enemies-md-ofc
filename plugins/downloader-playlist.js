8620930ebd3a75d778d753ca98e9b6f6286ae7fc
ok222901 <115047957+unlimited787@users.noreply.github.com>
2023-03-18 00:43:13 +0100

    Add files via upload

--- plugins/downloader-playlist.js
+++ plugins/downloader-playlist.js
@@ -23,19 +23,19 @@ const listSerch = []
 const listSerch2 = []
 const listSerch3 = []
 const listSerch4 = []
-let teskd = `Musica per: ${args.join(" ")}`
+let teskd = `${args.join(" ")}`
 const sections = [{ title: `AUDIO`, rows: listSerch }, { title: `VIDEO`, rows: listSerch2 }, { title: `DOCUMENTO MP3`, rows: listSerch3 }, { title: `DOCUMENTO MP4`, rows: listSerch4 }]
 for (let x of get_result2) {
 listSerch.push({title: x.title, description: null, rowId: `${usedPrefix}ytmp3 http://www.youtube.com/watch?v=${x.videoId}`})
 listSerch2.push({title: x.title, description: null, rowId: `${usedPrefix}ytmp4 http://www.youtube.com/watch?v=${x.videoId}`})
 listSerch3.push({title: x.title, description: null, rowId: `${usedPrefix}ytmp3doc http://www.youtube.com/watch?v=${x.videoId}`})
 listSerch4.push({title: x.title, description: null, rowId: `${usedPrefix}ytmp4doc http://www.youtube.com/watch?v=${x.videoId}`})}
-const listMessage = { text: teskd, footer: 'scegli una opzione', title: "Musica trovata", buttonText: "risultati", sections }
+const listMessage = { text: teskd, footer: '𝐬𝐞𝐥𝐞𝐳𝐢𝐨𝐧𝐚 𝐪𝐮𝐢 𝐬𝐨𝐭𝐭𝐨', title: "𝐌𝐮𝐬𝐢𝐜𝐚 𝐭𝐫𝐨𝐯𝐚𝐭𝐚", buttonText: "𝐫𝐢𝐬𝐮𝐥𝐭𝐚𝐭𝐢", sections }
 conn.sendMessage(m.chat, listMessage, { quoted: m })    
 } catch {    
 return
 }}}
-handler.command = /^playlist|playlist2$/i
+handler.command = /^playlist2|playlist$/i
 export default handler
 async function fetchJson(url, options) {
 try {
